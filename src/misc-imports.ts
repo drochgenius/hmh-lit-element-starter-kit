df267e9c84f8dbfee771619c4e25512b28e14b2e
Dominique Rochefort <dominique.rochefort@hmhco.com>
2019-01-24 14:19:06 -0500

    fix until() test

--- src/misc-imports.ts
+++ src/misc-imports.ts
@@ -1,2 +1,4 @@
 export * from '@polymer/iron-component-page';
 export * from '@hmh/lit-element-tester';
+// @ts-ignore: issue in @types/sinon definitions
+export * from 'sinon';
--- src/unit/my-component.spec.ts
+++ src/unit/my-component.spec.ts
@@ -1,9 +1,21 @@
-import { withSnippet} from '@hmh/lit-element-tester';
+import { withSnippet } from '@hmh/lit-element-tester';
+import { SinonStub, stub } from 'sinon';
 import { MyComponent } from '../components/my-component';
 
 const expect: Chai.ExpectStatic = chai.expect;
 
+const fakeQuote = 'There is nothing permanent except change.';
+
 describe(`<my-component>`, (): void => {
+    let fetchStub: SinonStub;
+
+    beforeEach(() => {
+        const responseData = {};
+        fetchStub = stub(window, 'fetch').returns(
+            Promise.resolve(new Response(JSON.stringify(responseData), { headers: { 'Content-Type': 'application/json' } }))
+        );
+    });
+
     it('should render default state', async (): Promise<void> => {
         withSnippet('default');
         const el: MyComponent = document.querySelector('my-component');
@@ -12,6 +24,16 @@ describe(`<my-component>`, (): void => {
         expect(el.shadowRoot).not.to.be.undefined;
         const heading: HTMLHeadingElement = el.shadowRoot.querySelector('h3');
         expect(heading.innerText).to.equal('Hello World!');
+        const paragraph: HTMLParagraphElement = el.shadowRoot.querySelector('p.quote');
+
+        expect(paragraph.innerText).to.deep.equal('loading...');
+
+        await new Promise((done: any) => {
+            setTimeout(() => {
+                expect(paragraph.innerText).to.deep.equal(fakeQuote);
+                done();
+            }, 0);
+        });
     });
 
     it('should render with provided name attribute', async (): Promise<void> => {
@@ -36,6 +58,10 @@ describe(`<my-component>`, (): void => {
         el.name = 'Jane';
         await el.updateComplete;
         expect(heading.innerText).to.equal('Hello Jane!');
+    });
+
+    afterEach(() => {
+        fetchStub.restore();
     });
 });
 
